0c1086ad564c258770a8af2af5b14f131079b292
Hector <141383937+hfelix3@users.noreply.github.com>
2024-02-01 21:28:03 -0700

    added components added pages folder with error fiel moved homepage file to pages folder

---
+++ client/src/pages/homepage.jsx
@@ -0,0 +1,18 @@
+// TODO: ADD STARTER CODE
+import { useQuery } from '@apollo/client';
+import Tasks from './components/Tasks';
+
+function Homepage({ TasksValue }) {
+
+    return(
+        <main>
+            <div>
+                <h1>Welcome!</h1>
+                <section class="contentSection">
+                {TasksValue ? <Tasks/> : null}
+                </section>
+            </div>
+        </main>
+    );
+}
+    export default Homepage;

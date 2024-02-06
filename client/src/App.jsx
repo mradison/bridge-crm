ac2bc703ef9f14e87fd0d825b350e16a6eee5b19
Hector <141383937+hfelix3@users.noreply.github.com>
2024-02-05 19:22:37 -0700

    added singnup login mutations and queries files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,45 @@
-import { useState } from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Homepage from './pages/homepage.jsx';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
 
   return (
-    <>
+    <ApolloProvider client={client}>
+      <Header />
       <Homepage />
-    </>
+    </ApolloProvider>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'
 import './index.css'
-import Error from './pages/Error'
-import Homepage from './pages/homepage.jsx';
 
+import App from './App.jsx'
+import Homepage from './pages/homepage.jsx';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Error from './pages/Error'
+
+import { RouterProvider } from 'react-router-dom/dist'
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -16,6 +20,12 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Homepage />,
+      },{
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
       },
 
     ],
@@ -26,4 +36,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <RouterProvider router={router} />
 )

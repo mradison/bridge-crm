25d23db3178104099194be55019585b04870d8db
Madison Reynolds <madison.reynolds@phoenix.edu>
2026-03-05 19:26:05 -0700

    Fixed MongoDB connection and env configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
+// server/config/connection.js
+require('dotenv').config({ path: require('path').resolve(__dirname, '..', '.env') });
 const mongoose = require('mongoose');
 
-const myuser = process.env.REACT_APP_MongoUser
-const mypass = process.env.REACT_APP_MongoPW
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://' + myuser + ':' + mypass + '@cluster0.wj6j9lt.mongodb.net/bridge-crm');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bridgeCRM';
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
